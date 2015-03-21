f6d02d1e51b5bf69993074251c824fdeba19dc16
Marc-André Stoppert <marc@egoserv.com>
2015-03-21 20:55:01 +0100

    --amend

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name	Kingsage Enhancement Suite
-// @version	1.3.0.2
+// @version	1.3.0.3
 // @author	grafilicious
 // @include http://*.kingsage.*/game.php*
 // @include	http://*.kingsage.*/redir.php*
