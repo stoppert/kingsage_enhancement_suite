dfaef13aa622d0c7cb6d17b0303e24660c320b85
Marc-André Stoppert <marc@egoserv.com>
2015-06-25 19:08:51 +0200

    Bump version fix problem with tampermonkey not recognizing the include for some reasons

--- src/header.js
+++ src/header.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name	Kingsage Enhancement Suite
-// @version	1.3.0.4
+// @version	1.3.0.5
 // @author	grafilicious
-// @include http://*.kingsage.*/game.php*
-// @include	http://*.kingsage.*/redir.php*
+// @include http://*.kingsage.*game.php*
+// @include http://*.kingsage.*redir.php*
 // @exclude	http://support.*/*
 // @grant	none
 // ==/UserScript==
